c4c4f166a9c1bfba316b35b87dad004d7525d150
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-06-18 20:17:16 +0300

    Add files via upload

--- html/chipbot/js/script.js
+++ html/chipbot/js/script.js
@@ -17,7 +17,7 @@ const start = () => {
 
 popup.addEventListener('click', () => {
     popup.classList.remove('active');
-})
+});
 
 // Tariffs btn
 
@@ -32,8 +32,30 @@ const year = () => {
     console.log('clk');
 }
 
+//  DropDown
 
+let dropdownHeader = document.querySelectorAll(".faq__dropdown_block_header");
+let dropdownMain = document.querySelectorAll(".faq__dropdown_block_main");
 
+if(dropdownHeader){
+    for (let i = 0; i < dropdownHeader.length; i++) {
+        dropdownHeader[i].addEventListener('click',()=>{
+            if(dropdownHeader[i].classList.contains("faq__dropdown_block_header-active")){
+                dropdownHeader[i].classList.remove("faq__dropdown_block_header-active");
+                dropdownMain[i].classList.remove("faq__dropdown_block_main-active");
+            }else{
+                dropdownHeader[i].classList.add("faq__dropdown_block_header-active");
+                dropdownMain[i].classList.add("faq__dropdown_block_main-active");
+                for (let b = 0; b < dropdownHeader.length; b++) {
+                    if(b != i){
+                        dropdownHeader[b].classList.remove("faq__dropdown_block_header-active");
+                        dropdownMain[b].classList.remove("faq__dropdown_block_main-active"); 
+                    }
+                }
+            }
+        });
+    }
+}
 
 
 
