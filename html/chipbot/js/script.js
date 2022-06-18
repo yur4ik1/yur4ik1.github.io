46965632dc4466dbd441cac5b16bb29f0933d063
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-06-18 13:51:05 +0300

    Add files via upload

--- html/chipbot/js/script.js
+++ html/chipbot/js/script.js
@@ -1,8 +1,44 @@
+// burger
+
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('remove');
+    burgerMenu.classList.toggle('active');
+});
+
+// popup
+
+const popup = document.querySelector('.popup');
+const start = () => {
+    popup.classList.add('active');
+};
+
+popup.addEventListener('click', () => {
+    popup.classList.remove('active');
+})
+
+// tariff btn
 
 const tariffsBtn = document.querySelector('.tariffs-btn');
 const bool = document.querySelector('.bool');
+let tariffsPrice = document.querySelector('.tariffs-price');
 
 tariffsBtn.addEventListener('click', () => {
     bool.classList.toggle('active');
-    console.log('click');
+
 });
+
+const year = () => {
+    document.querySelector('.tariffs-price1').classList.toggle("year");
+    document.querySelector('.tariffs-price2').classList.toggle("year");
+    console.log('click');
+}
+
+
+
+
+
+
+
