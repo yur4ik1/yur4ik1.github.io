8c644c18f6debb3f795f4f653dbe3491dd33e161
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-07-11 22:13:44 +0300

    Add files via upload

--- html/MySite/js/script.js
+++ html/MySite/js/script.js
@@ -59,7 +59,7 @@ window.onscroll = () => {
 let bg = document.querySelector(".intro-back");
 window.addEventListener("scroll", function(){
     bg.style.backgroundSize = 90 + +window.pageYOffset/30+"%";
-    bg.style.top = -0 - +window.pageYOffset/6+"px";
+    bg.style.top = -0 - +window.pageYOffset/4+"px";
     bg.style.height = 1000 + +window.pageYOffset/11+"px";
 })
 
