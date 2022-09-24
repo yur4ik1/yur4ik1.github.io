453353af62f6ba69f82a809543a57128028faec9
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-09-24 13:13:31 +0300

    Update script.js

--- html/Site24/js/script.js
+++ html/Site24/js/script.js
@@ -1,9 +1,4 @@
-console.log('%cDeveloped by Djigit & yur4ik96', 'color: #c434c4; font-size: 50px ');
 
-console.log(
-  '%cTG: @Djigit34 / @yura_k18',
-  'color: #c434c4; background: #eee; font-size: 30px '
-);
 
 
 const hasChidItem = document.querySelectorAll(".has-child");
