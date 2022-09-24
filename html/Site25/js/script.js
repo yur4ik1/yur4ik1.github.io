431ba77ebbab1f2e33822cf7a8f87c348c577522
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-09-24 13:14:01 +0300

    Update script.js

--- html/Site25/js/script.js
+++ html/Site25/js/script.js
@@ -1,9 +1,3 @@
-console.log('%cDeveloped by Djigit & yur4ik96', 'color: #c434c4; font-size: 50px ');
-
-console.log(
-  '%cTG: @Djigit34 / @yura_k18',
-  'color: #c434c4; background: #eee; font-size: 30px '
-);
 
 
 // burger
