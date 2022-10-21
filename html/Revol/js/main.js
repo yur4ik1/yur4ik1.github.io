ce88e16fae8b9dacec6676f69b2995c320075d65
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-10-21 22:21:57 +0300

    Add files via upload

--- html/Revol/js/main.js
+++ html/Revol/js/main.js
@@ -1,5 +1,3 @@
-
-
 // feedback animation, Scroll header
 
 const width = window.innerWidth
@@ -97,3 +95,20 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 
 });
+
+// Scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
