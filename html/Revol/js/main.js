46c74251d43b7bf2abd1b35e514afd9d1b7fe01c
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-10-21 12:22:24 +0300

    Add files via upload

--- html/Revol/js/main.js
+++ html/Revol/js/main.js
@@ -10,7 +10,23 @@ window.onscroll = () => {
     if (posTop > 1700) {
         window.addEventListener("scroll", () => {
             bg.style.transform = 'rotate(' + 100 +window.pageYOffset / 50 + 'deg)';
-            console.log('111')
         });
     }
 }
+
+// Tabs
+
+document.addEventListener('DOMContentLoaded', function(e){ 'use strict';
+    let list = document.querySelectorAll('#tabNav a');
+    list = Array.prototype.slice.call(list, 0);
+    list.forEach(function(el, i, ar) {
+        el.addEventListener('click', function(event){
+            e.preventDefault();
+            let tab = document.querySelector(el.getAttribute('href'));
+            document.querySelector('#tabNav .act').classList.remove('act');
+            document.querySelector('#tabsWrap .act').classList.remove('act');
+            el.classList.add('act');
+            tab.classList.add('act');
+        })
+    })
+})
