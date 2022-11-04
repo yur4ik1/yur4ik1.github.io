6cf08cfc3748d3b5aedfa196fa5356c129c97d2d
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-11-04 22:47:23 +0200

    Add files via upload

--- html/cleaner/js/main.js
+++ html/cleaner/js/main.js
@@ -37,3 +37,55 @@ iBtn.addEventListener('click', () => {
     iBtn.classList.toggle('active');
     star.classList.toggle('active');
 });
+
+
+const arrow1 = document.querySelector('.work__top-arrow-1');
+const arrow2 = document.querySelector('.work__top-arrow-2');
+const arrow3 = document.querySelector('.work__top-arrow-3');
+const arrow4 = document.querySelector('.work__top-arrow-4');
+const rightImg1 = document.querySelector('.work__right-img-1');
+const rightImg2 = document.querySelector('.work__right-img-2');
+const rightImg3 = document.querySelector('.work__right-img-3');
+const rightImg4 = document.querySelector('.work__right-img-4');
+
+
+arrow1.addEventListener('mouseover', () => {
+    rightImg1.classList.add('active');
+    rightImg2.classList.remove('active');
+    rightImg3.classList.remove('active');
+    rightImg4.classList.remove('active');
+    arrow1.classList.add('active');
+    arrow2.classList.remove('active');
+    arrow3.classList.remove('active');
+    arrow4.classList.remove('active');
+});
+arrow2.addEventListener('mouseover', () => {
+    rightImg2.classList.add('active');
+    rightImg1.classList.remove('active');
+    rightImg3.classList.remove('active');
+    rightImg4.classList.remove('active');
+    arrow2.classList.add('active');
+    arrow1.classList.remove('active');
+    arrow3.classList.remove('active');
+    arrow4.classList.remove('active');
+});
+arrow3.addEventListener('mouseover', () => {
+    rightImg3.classList.add('active');
+    rightImg2.classList.remove('active');
+    rightImg1.classList.remove('active');
+    rightImg4.classList.remove('active');
+    arrow3.classList.add('active');
+    arrow2.classList.remove('active');
+    arrow1.classList.remove('active');
+    arrow4.classList.remove('active');
+});
+arrow4.addEventListener('mouseover', () => {
+    rightImg4.classList.add('active');
+    rightImg2.classList.remove('active');
+    rightImg3.classList.remove('active');
+    rightImg1.classList.remove('active');
+    arrow4.classList.add('active');
+    arrow2.classList.remove('active');
+    arrow3.classList.remove('active');
+    arrow1.classList.remove('active');
+});
