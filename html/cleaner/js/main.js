d86e3acbd4d2fda840d23c4ccd0573a82458004e
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-11-05 11:20:36 +0200

    Add files via upload

--- html/cleaner/js/main.js
+++ html/cleaner/js/main.js
@@ -89,3 +89,20 @@ arrow4.addEventListener('mouseover', () => {
     arrow3.classList.remove('active');
     arrow1.classList.remove('active');
 });
+
+// Scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
