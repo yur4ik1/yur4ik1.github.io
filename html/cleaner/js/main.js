25227e1cd7be0d1f168bfd7e75ce4b1850409635
Yura <46454397+yur4ik1@users.noreply.github.com>
2022-11-06 23:42:25 +0200

    Add files via upload

--- html/cleaner/js/main.js
+++ html/cleaner/js/main.js
@@ -107,6 +107,24 @@ for (let anchor of anchors) {
   })
 }
 
+// Tabs
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    'use strict';
+    let list = document.querySelectorAll('#tabNav a');
+    list = Array.prototype.slice.call(list, 0);
+    list.forEach(function (el, i, ar) {
+        el.addEventListener('click', function (event) {
+            e.preventDefault();
+            let tab = document.querySelector(el.getAttribute('href'));
+            document.querySelector('#tabNav .act').classList.remove('act');
+            document.querySelector('#tabsWrap .act').classList.remove('act');
+            el.classList.add('act');
+            tab.classList.add('act');
+        })
+    })
+});
+
 // Mask
 
 window.addEventListener("DOMContentLoaded", function() {
