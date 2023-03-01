8ec427b8fb583432b352919697d52310ebec5628
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-03-01 20:45:36 +0200

    Add files via upload

--- html/kidztype-rework/js/main.js
+++ html/kidztype-rework/js/main.js
@@ -65,5 +65,5 @@ if (document.querySelector('.games-type') !== null) {
 setTimeout(function() {
   var dataUrl = document.getElementById('iframeID').getAttribute('data-url');
   document.getElementById('iframeID').src = dataUrl;
-}, 2000);
+}, 1500);
 
