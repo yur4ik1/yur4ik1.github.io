521534b5c95e74ecba59f16be4bee5ccadbd8f01
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-03-01 15:36:07 +0200

    Add files via upload

--- html/kidztype-rework/js/main.js
+++ html/kidztype-rework/js/main.js
@@ -1,3 +1,66 @@
+
+// mobile menu
+let burger = document.querySelector('.header__burger');
+let mobileMenu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  mobileMenu.classList.toggle('mobile-menu');
+});
+
+// scroll top btn
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
+}
+
+let button = document.getElementById("toTop");
+
+button.addEventListener("click", function () {
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollToTop);
+  }
+});
+
+function scrollToTop() {
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, currentScroll - (currentScroll / 8));
+  }
+}
+
+if (document.querySelector('.games-type') !== null) {
+  // grig btn
+  let btnGrid = document.querySelector('.btn-grid');
+  let btnList = document.querySelector('.btn-list');
+  let gamesList = document.querySelectorAll('.games');
+
+  btnList.addEventListener('click', () => {
+    btnGrid.classList.remove('active');
+    btnList.classList.add('active');
+    gamesList.forEach(game => {
+      game.classList.add('list');
+    });
+  });
+
+  btnGrid.addEventListener('click', () => {
+    btnGrid.classList.add('active');
+    btnList.classList.remove('active');
+    gamesList.forEach(game => {
+      game.classList.remove('list');
+    });
+  });
+}
+
+// like animations
+
 if (document.querySelector('.games-admin') !== null) {
   // Get all elements with class "like-cnt"
   const likeCnts = document.querySelectorAll('.heart .like-cnt');
@@ -214,55 +277,3 @@ if (document.querySelector('.games-admin') !== null) {
   });
 
 }
-
-// scroll top btn
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("toTop").style.display = "block";
-  } else {
-    document.getElementById("toTop").style.display = "none";
-  }
-}
-
-let button = document.getElementById("toTop");
-
-button.addEventListener("click", function () {
-  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (currentScroll > 0) {
-    window.requestAnimationFrame(scrollToTop);
-  }
-});
-
-function scrollToTop() {
-  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (currentScroll > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, currentScroll - (currentScroll / 8));
-  }
-}
-
-if (document.querySelector('.games-type') !== null) {
-  // grig btn
-  let btnGrid = document.querySelector('.btn-grid');
-  let btnList = document.querySelector('.btn-list');
-  let gamesList = document.querySelectorAll('.games');
-
-  btnList.addEventListener('click', () => {
-    btnGrid.classList.remove('active');
-    btnList.classList.add('active');
-    gamesList.forEach(game => {
-      game.classList.add('list');
-    });
-  });
-
-  btnGrid.addEventListener('click', () => {
-    btnGrid.classList.add('active');
-    btnList.classList.remove('active');
-    gamesList.forEach(game => {
-      game.classList.remove('list');
-    });
-  });
-}
-
