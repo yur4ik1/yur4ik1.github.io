75959fc188d13420ad6cbc8e6540e9346dd64869
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-03-30 18:21:24 +0300

    Add files via upload

---
+++ html/tensig/contact-form.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'your_email@example.com';
+    $subject = 'New Contact Request';
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message!";
+    } else {
+        echo "Sorry, there was a problem sending your message.";
+    }
+}
+?>
--- html/tensig/js/main.js
+++ html/tensig/js/main.js
@@ -1,3 +1,11 @@
+//  Burger menu
+
+let mobileMenu = document.querySelector('.mobile__menu');
+const burger = () => {
+    mobileMenu.classList.toggle('active');
+};
+
+
 // feedback animation, Scroll header
 
 const width = window.innerWidth
@@ -72,10 +80,26 @@ for (let anchor of anchors) {
 }
 
 
-//  Burger menu
-
-let mobileMenu = document.querySelector('.mobile__menu');
-
-const burger = () => {
-    mobileMenu.classList.toggle('active');
-};
+var form = document.getElementById('contact__form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData(form);
+    xhr.open('POST', '../contact-form.php', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            form.reset();
+            var response = xhr.responseText;
+            var successMessage = document.createElement('p');
+            successMessage.classList.add('success');
+            successMessage.textContent = response;
+            form.appendChild(successMessage);
+        } else {
+            var errorMessage = document.createElement('p');
+            errorMessage.classList.add('error');
+            errorMessage.textContent = 'Sorry, there was a problem sending your message.';
+            form.appendChild(errorMessage);
+        }
+    };
+    xhr.send(formData);
+});
