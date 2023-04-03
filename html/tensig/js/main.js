d663f05aad4d3071c4800dfd84da8ba15e28329c
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-04-03 18:17:43 +0300

    Add files via upload

--- html/tensig/js/main.js
+++ html/tensig/js/main.js
@@ -93,23 +93,6 @@ videoBack.addEventListener('click', () => {
   popupVideo.style.display = 'none';
 });
 
-// Scroll
-
-const anchors = document.querySelectorAll('a[href*="#"]')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
-
 
 function sendData(form, phpFile) {
   form.addEventListener('submit', function(event) {
