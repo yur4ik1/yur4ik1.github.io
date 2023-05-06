dd30e8fd51991360cf4eae959ce09e5c7718e950
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-05-06 17:10:05 +0300

    Add files via upload

---
+++ html/ninjable-portal/js/main.js
@@ -0,0 +1,17 @@
+
+/* passwordInput */
+
+const passwordInput = document.getElementById("password");
+const passwordIcon = document.querySelector(".password-icon");
+
+if (passwordInput && passwordIcon) {
+  passwordIcon.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      passwordIcon.classList.add('active');
+    } else {
+      passwordInput.type = "password";
+      passwordIcon.classList.remove('active');
+    }
+  })
+};
