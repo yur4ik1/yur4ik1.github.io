22dc1bdc99ed2d2ef3f99c219398faccf0725a5f
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-05-23 16:52:36 +0300

    Add files via upload

--- html/raisini/js/main.js
+++ html/raisini/js/main.js
@@ -1,3 +1,5 @@
+// mobile menu
+
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.mobile__menu');
 const burgerClose = document.querySelector('.burger-close');
@@ -12,18 +14,48 @@ burgerClose.addEventListener('click', () => {
 
 // header
 
-var prevScrollPos = window.pageYOffset;
+const prevScrollPos = window.pageYOffset;
 
 window.addEventListener('scroll', function() {
-  var currentScrollPos = window.pageYOffset;
-
+  const currentScrollPos = window.pageYOffset;
   if (prevScrollPos > currentScrollPos) {
-    // Прокручено вгору
     document.querySelector('.header').classList.remove('hidden');
   } else {
-    // Прокручено вниз
     document.querySelector('.header').classList.add('hidden');
   }
-
   prevScrollPos = currentScrollPos;
 });
+
+// filter btns
+
+document.addEventListener('DOMContentLoaded', function() {
+    const filterButton = document.querySelector('.filter');
+    const filterList = document.querySelector('.filter-list');
+    const sortButton = document.querySelector('.sort');
+    const sortList = document.querySelector('.sort-list');
+
+    filterButton.addEventListener('click', function() {
+      filterList.classList.toggle('active');
+      sortList.classList.remove('active'); 
+    });
+
+    sortButton.addEventListener('click', function() {
+      sortList.classList.toggle('active');
+      filterList.classList.remove('active'); 
+    });
+
+    document.addEventListener('click', function(event) {
+      const target = event.target;
+      if (
+        target !== filterButton &&
+        target !== sortButton &&
+        target !== filterList &&
+        target !== sortList &&
+        !filterList.contains(target) &&
+        !sortList.contains(target)
+      ) {
+        filterList.classList.remove('active');
+        sortList.classList.remove('active');
+      }
+    });
+  });
