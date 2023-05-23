ae4502ad83890d8833c8816b2217a53c6802eec1
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-05-23 15:20:33 +0300

    Add files via upload

--- html/raisini/js/main.js
+++ html/raisini/js/main.js
@@ -9,3 +9,22 @@ burger.addEventListener('click', () => {
 burgerClose.addEventListener('click', () => {
     mobileMenu.classList.remove('active');
 });
+
+// header
+
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('.header');
+    var scrollPos = window.pageYOffset;
+
+    var opacity = 1 - scrollPos / 200; // Залежно від прокрутки, змінюємо значення прозорості
+
+    if (opacity < 0) {
+        // Прозорість досягла 0
+        header.style.opacity = 0;
+        header.classList.add('hidden');
+    } else {
+        // Прозорість все ще більше 0
+        header.style.opacity = opacity;
+        header.classList.remove('hidden');
+    }
+});
