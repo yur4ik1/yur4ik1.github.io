a153b5f9e59d1b370a67ae735763c8679ce3bfea
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-08-27 18:31:47 +0300

    Add files via upload

--- html/SOAX/js/main.js
+++ html/SOAX/js/main.js
@@ -5,20 +5,20 @@ const header = document.querySelector(".header__main");
 const width = window.innerWidth
 
 window.onscroll = () => {
-    let posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    if (posTop > 1) {
-        header.classList.add('active');
-    } else {
-        header.classList.remove('active');
-    }
+  let posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  if (posTop > 1) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
 
-    if (width < 1023) {
-        if (posTop > 0) {
-            header.classList.add('active');
-        } else {
-            header.classList.remove('active');
-        }
+  if (width < 1023) {
+    if (posTop > 0) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
     }
+  }
 
 }
 
@@ -28,79 +28,122 @@ window.onscroll = () => {
 // ——————————————————————————————————————————————————
 
 class TextScramble {
-    constructor(el) {
-      this.el = el
-      this.chars = 'xoyof'
-      this.update = this.update.bind(this)
+  constructor(el) {
+    this.el = el
+    this.chars = 'xoyof'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
     }
-    setText(newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 40)
-        const end = start + Math.floor(Math.random() * 40)
-        this.queue.push({ from, to, start, end })
-      }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
-    }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="dud">${char}</span>`
-        } else {
-          output += from
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
         }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
+        output += `<span class="dud">${char}</span>`
       } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
+        output += from
       }
     }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
     }
   }
-  
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
-  
-  const phrases = [
-    'xoyof',
-  ]
-  
-  const el = document.querySelector('.gray-text')
-  const fx = new TextScramble(el)
-  
-  let counter = 0
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800)
-    })
-    counter = (counter + 1) % phrases.length
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
   }
-  
-  next()
+}
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+const phrases = [
+  'xoyof',
+]
+
+const el = document.querySelector('.gray-text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
 
 
+
+// Products Tabs 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectItems = document.querySelectorAll(".products__tabs-item");
+  const tabsItems = document.querySelectorAll(".tabs-item");
+
+  selectItems.forEach((selectItem, index) => {
+    selectItem.addEventListener("click", () => {
+      selectItems.forEach(item => {
+        item.classList.remove("active");
+      });
+      selectItem.classList.add("active");
+      tabsItems.forEach(item => {
+        item.classList.remove("active");
+      });
+      tabsItems[index].classList.add("active");
+    });
+  });
+});
+
+
+// Plans Dropdown 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const listItems = document.querySelectorAll(".plans__list-item");
+
+  listItems.forEach(function (item) {
+    const head = item.querySelector(".plans__list-head");
+
+    head.addEventListener("click", function () {
+      // Забираємо всі інші активні класи
+      listItems.forEach(function (otherItem) {
+        if (otherItem !== item) {
+          otherItem.classList.remove("active");
+        }
+      });
+
+      // Додаємо або видаляємо клас active
+      item.classList.toggle("active");
+    });
+  });
+});
 
