1aff0c1336b156dd65aab87a53b76a2d61dcfb14
Yura <46454397+yur4ik1@users.noreply.github.com>
2023-11-18 13:30:57 +0200

    Add files via upload

--- html/SkyGrid/js/main.js
+++ html/SkyGrid/js/main.js
@@ -1,13 +1,41 @@
+// mobile menu
+
+let burder = document.querySelector('.burger');
+let mobileMenu = document.querySelector('.header__mobile-menu');
+let headerMain = document.querySelector('.header__main')
+
+if (mobileMenu) {
+  burder.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    burder.classList.toggle('active');
+    headerMain.classList.toggle('act');
+
+    if (mobileMenu.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  });
+}
+
+
 // price btn
 
 let retailText = document.querySelector('.retail-text');
 let wholesaleText = document.querySelector('.wholesale-text');
 let filterBtn = document.querySelector('.price__filter-btn');
+let priceCards = document.querySelectorAll('.price__cards');
 
 if (filterBtn) {
     filterBtn.addEventListener('click', () => {
         retailText.classList.toggle('active');
         filterBtn.classList.toggle('active');
         wholesaleText.classList.toggle('active');
-    })
+
+        // Перевіряємо, який елемент price__cards вже має клас 'active'
+        priceCards.forEach((card) => {
+            // Перемикаємо клас 'active' для кожного елемента
+            card.classList.toggle('active');
+        });
+    });
 }
