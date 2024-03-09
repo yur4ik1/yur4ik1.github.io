bba0ee59ffee89da9be7f78ca9e990db62a7c20e
Yurii <46454397+yur4ik1@users.noreply.github.com>
2024-03-09 20:20:20 +0200

    Add files via upload

---
+++ html/scss/js/main.js
@@ -0,0 +1,33 @@
+/*
+// Scroll header
+
+const header = document.querySelector(".header__main");
+const width = window.innerWidth
+
+if (header) {
+  window.onscroll = () => {
+    let posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    if (posTop > 1) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
+    }
+
+    if (width < 1023) {
+      if (posTop > 0) {
+        header.classList.add('active');
+      } else {
+        header.classList.remove('active');
+      }
+    }
+
+  }
+} */
+
+const menuBurger = document.querySelector('.header__burger');
+const mobileNavigation = document.querySelector('.mobile-navigation');
+
+menuBurger.addEventListener('click', () => {
+  mobileNavigation.classList.toggle('active');
+  menuBurger.classList.toggle('active');
+});
